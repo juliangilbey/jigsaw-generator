8d27f450f961d27256f66149d66c82e346a1c2fc
Julian Gilbey <julian-git@d-and-j.net>
2014-05-30 13:28:36 +0100

    Checkpoint - beginning to think about modularisation

--- jigsaw-generate.py
+++ jigsaw-generate.py
@@ -6,6 +6,10 @@ import os
 import re
 import argparse
 
+# These modules handle the different types of puzzle: jigsaw, card sort
+# and dominoes
+import jigsaw.jigsaw
+
 from yaml import load, dump
 try:
     from yaml import CLoader as Loader, CDumper as Dumper
@@ -13,8 +17,8 @@ except ImportError:
     from yaml import Loader, Dumper
 
 parser = argparse.ArgumentParser()
-parser.add_argument("puzfile", metavar="puzzlefile[.yaml]",
-                    help="yaml file containing puzzle data")
+parser.add_argument('puzfile', metavar='puzzlefile[.yaml]',
+                    help='yaml file containing puzzle data')
 args = parser.parse_args()
 if args.puzfile[-5:] == '.yaml':
     puzfile = args.puzfile
@@ -22,50 +26,120 @@ else:
     puzfile = args.puzfile + '.yaml'
 puzbase = puzfile[:-5]
 
-knowntypes = {
-    'smallhexagon': True
-}
-
-def losub(text, subs):
-    """Substitute <:= var :> strings in text using the dict subs"""
-    def subtext(matchobj):
-        if matchobj.group(1) in subs:
-            return subs[matchobj.group(1)]
-        else:
-            print("Unrecognised substitution: %s" % matchobj.group(0),
-                  file=sys.stderr)
-    return re.sub(r"<:=\s*(\S*)\s*:>", subtext, text)
-
-# This will be taken from command line parameter soon!
 try:
     infile = open(puzfile)
 except:
-    sys.exit("Cannot open %s for reading" % puzfile)
+    sys.exit('Cannot open %s for reading' % puzfile)
 
-headerf = open("template-header.tex")
-bodytablef = open("template-table.tex")
+try:
+    data = load(infile, Loader=Loader)
+except yaml.YAMLError, exc:
+    if hasattr(exc, 'problem_mark'):
+        mark = exc.problem_mark
+        sys.exit('Error parsing puzzle data file\nError position: line %s, column %s' % (mark.line+1, mark.column+1))
+else:
+    sys.exit('Error parsing data file: %s' % exc)
 
-data = load(infile, Loader=Loader)
+knowntypes = {
+    'smallhexagon': jigsaw.jigsaw
+}
 
 if 'type' in data and data['type'] in knowntypes:
     puztype = data['type']
 elif 'type' in data:
-    sys.exit("Unrecognised jigsaw type %s" % data['type'])
+    sys.exit('Unrecognised jigsaw type %s' % data['type'])
 else:
-    sys.exit("No jigsaw type found in puzzle file")
+    sys.exit('No jigsaw type found in puzzle file')
 
-layoutf = open(puztype + ".yaml")
-bodypuzf = open("template-" + puztype + "-puzzle.tex")
-bodysolf = open("template-" + puztype + "-solution.tex")
+#########################
+
+
+# LaTeX font sizes
+sizes = [r'\tiny',
+         r'\scriptsize',
+         r'\footnotesize',
+         r'\small',
+         r'\normalsize',
+         r'\large',
+         r'\Large',
+         r'\LARGE',
+         r'\huge',
+         r'\Huge'
+         ]
+
+def losub(text, subs):
+    """Substitute <: var :> strings in text using the dict subs"""
+    def subtext(matchobj):
+        if matchobj.group(1) in subs:
+            return subs[matchobj.group(1)]
+        else:
+            print('Unrecognised substitution: %s' % matchobj.group(0),
+                  file=sys.stderr)
+    return re.sub(r'<:\s*(\S*)\s*:>', subtext, text)
+
+def make_entry(entry, defaultsize, hide):
+    """Convert a YAML entry into a LaTeX-formatted entry
+
+    The YAML entry will either be a simple text entry, or it will be a
+    dictionary with required key "text" and optional entries "size"
+    and "hidden".
+
+    In the latter case, the result will be an empty string if "hidden"
+    (from the YAML file) is true and the make_entry parameter hide is
+    True, otherwise the text will be output.
+
+    If there is a "size" key, this will be added to the defaultsize.
+    The output will have the text with the appropriate LaTeX size
+    command prepended."""
+
+    if isinstance(entry, dict):
+        if 'text' not in entry:
+            print('No "text" field in entry in data file.  Rest of data is:\n',
+                  file=sys.stderr)
+            for f in entry:
+                print('  %s: %s\n' % (f, entry[f]), file=sys.stderr)
+            return ''
+            
+        if hide and 'hidden' in entry and entry['hidden']:
+            return ''
+        if 'size' in entry:
+            try:
+                size = defaultsize + int(entry['size'])
+                if size < 0:
+                    size = 0
+                if size >= len(sizes):
+                    size = len(sizes) - 1
+            except:
+                print('Unrecognised size entry for text %(text)s:\n'
+                      'size = %(size)s\n'
+                      'Defaulting to default size\n' %
+                      entry, file=sys.stderr)
+                size = defaultsize
+        else:
+            size = defaultsize
+
+        return "%s %s" % (sizes[size], entry['text'])
+    else:
+        return "%s %s" % (sizes[defaultsize], entry)
+
+
+headerf = open('template-header.tex')
+bodytablef = open('template-table.tex')
+
+
+
+layoutf = open(puztype + '.yaml')
+bodypuzf = open('template-' + puztype + '-puzzle.tex')
+bodysolf = open('template-' + puztype + '-solution.tex')
 layout = load(layoutf, Loader=Loader)
 
 bodypuz = bodypuzf.read()
 bodysol = bodysolf.read()
 bodytable = bodytablef.read()
 
-outpuz = open(puzbase + "-puzzle.tex", "w")
-outsol = open(puzbase + "-solution.tex", "w")
-outtable = open(puzbase + "-table.tex", "w")
+outpuz = open(puzbase + '-puzzle.tex', 'w')
+outsol = open(puzbase + '-solution.tex', 'w')
+outtable = open(puzbase + '-table.tex', 'w')
 
 header = headerf.read()
 print(header, file=outpuz)
@@ -82,42 +156,52 @@ else:
     dsubs['title'] = ''
 random.seed(dsubs['title'])
 
+if 'puzzleTextSize' in data:
+    puzzleTextSize = data['puzzleTextSize']
+else:
+    puzzleTextSize = layout['puzzleTextSize']
+
+if 'solutionTextSize' in data:
+    solutionTextSize = data['solutionTextSize']
+else:
+    solutionTextSize = layout['solutionTextSize']
+
 # Read the card content
 # Three types of cards: pairs, edges, cards (which are single cards
 # for sorting activities)
 if 'pairs' in layout:
     if 'pairs' in data:
         pairs = data['pairs']
-        if layout['pairs'] == 0:
+        if layout['pairs'] == 0:  # which means any number of pairs
             if len(pairs) == 0:
-                sys.exit("Puzzle type %s needs at least one pair" %
+                sys.exit('Puzzle type %s needs at least one pair' %
                          layout['typename'])
         else:
             if len(pairs) != layout['pairs']:
-                sys.exit("Puzzle type %s needs exactly %s pairs" %
+                sys.exit('Puzzle type %s needs exactly %s pairs' %
                          (layout['typename'], layout['pairs']))
     else:
-        sys.exit("Puzzle type %s requires pairs in data file" %
+        sys.exit('Puzzle type %s requires pairs in data file' %
                  layout['typename'])
 elif 'pairs' in data:
-    sys.exit("Puzzle type %s does not accept pairs in data file" %
+    sys.exit('Puzzle type %s does not accept pairs in data file' %
              layout['typename'])
 
 if 'edges' in layout:
     if 'edges' in data:
         edges = data['edges']
         if len(edges) > layout['edges']:
-            print("Warning: more than %s edges given; extra will be ignored" %
+            print('Warning: more than %s edges given; extra will be ignored' %
                   layout['edges'], file=sys.stderr)
             edges = edges[:layout['edges']]
         elif len(edges) < layout['edges']:
-            print("Warning: fewer than %s edges given; remainder will be blank"
+            print('Warning: fewer than %s edges given; remainder will be blank'
                   % layout['edges'], file=sys.stderr)
-            edges += [""] * (layout['edges'] - len(edges))
+            edges += [''] * (layout['edges'] - len(edges))
     else:
-        edges = [""] * layout['edges']
+        edges = [''] * layout['edges']
 elif 'edges' in data:
-    sys.exit("Puzzle type %s does not accept edges in data file" %
+    sys.exit('Puzzle type %s does not accept edges in data file' %
              layout['typename'])
 
 if 'cards' in layout:
@@ -125,17 +209,17 @@ if 'cards' in layout:
         cards = data['cards']
         if layout['cards'] == 0:
             if len(cards) == 0:
-                sys.exit("Puzzle type %s needs at least one cards" %
+                sys.exit('Puzzle type %s needs at least one card' %
                          layout['typename'])
         else:
             if len(cards) != layout['cards']:
-                sys.exit("Puzzle type %s needs exactly %s cards" %
+                sys.exit('Puzzle type %s needs exactly %s cards' %
                          (layout['typename'], layout['cards']))
     else:
-        sys.exit("Puzzle type %s requires cards in data file" %
+        sys.exit('Puzzle type %s requires cards in data file' %
                  layout['typename'])
 elif 'cards' in data:
-    sys.exit("Puzzle type %s does not accept cards in data file" %
+    sys.exit('Puzzle type %s does not accept cards in data file' %
              layout['typename'])
 
 
@@ -145,12 +229,12 @@ dsubs['tablecards'] = ''
 
 for p in pairs:
     dsubs['tablepairs'] += (r'%s&%s\\ \hline%s' %
-                            (make_entry(p[0]), make_entry(p[1]), "\n"))
+                            (make_entry(p[0]), make_entry(p[1]), '\n'))
 for e in edges:
-    dsubs['tableedges'] += r'\strut %s\\ \hline%s' % (make_entry(e), "\n")
+    dsubs['tableedges'] += r'\strut %s\\ \hline%s' % (make_entry(e), '\n')
 
 for c in cards:
-    dsubs['tablecards'] += r'\strut %s\\ \hline%s' % (make_entry(c), "\n")
+    dsubs['tablecards'] += r'\strut %s\\ \hline%s' % (make_entry(c), '\n')
 
 for i in range(6):
     if random.choice([True, False]):
@@ -186,7 +270,7 @@ random.shuffle(triangleorder)
 # underline 6 and 9
 def cardnum(n):
     if n in [6, 9]:
-        return r'\underline{%d}' % n
+        return r'\underline{%s}' % n
     else:
         return str(n)
 
@@ -214,11 +298,11 @@ for i in range(6):
 
 # Testing:
 # for i in range(6):
-#     print("Sol card %d: (%s, %s, %s), num angle %d" %
+#     print('Sol card %s: (%s, %s, %s), num angle %s' %
 #            (i, trianglesolcard[i][0], trianglesolcard[i][1], trianglesolcard[i][2], trianglesolcard[i][4]))
 # 
 # for i in range(6):
-#     print("Puz card %d: (%s, %s, %s), num angle %d" %
+#     print('Puz card %s: (%s, %s, %s), num angle %s' %
 #            (i, trianglepuzcard[i][0], trianglepuzcard[i][1], trianglepuzcard[i][2], trianglepuzcard[i][3]))
 
 btext = losub(bodytable, dsubs)
